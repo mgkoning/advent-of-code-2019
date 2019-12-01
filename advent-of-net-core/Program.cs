a5f1fd3e29ed629a0eb72733f5fa01f1b1d15526
Michiel Koning <m.g.koning@gmail.com>
2019-12-01 09:42:36 +0100

    basic c# setup

---
+++ advent-of-net-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent_of_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
