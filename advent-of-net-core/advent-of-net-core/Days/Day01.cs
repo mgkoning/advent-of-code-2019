9d743cfc1766c7c6345658f645c6c5b0b3934cbd
Michiel Koning <m.g.koning@gmail.com>
2019-12-03 22:12:46 +0100

    C# Day01

---
+++ advent-of-net-core/advent-of-net-core/Days/Day01.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019 {
+  class Day01 {
+
+    internal async Task solve() {
+      await File.ReadAllLinesAsync("../input/day01.txt")
+        .ContinueWith(async linesTask => { 
+          var (part1, part2) = runPuzzle(linesTask.Result.Select(int.Parse).ToList());
+          await Console.Out.WriteLineAsync($"Part 1:\n{part1}");
+          await Console.Out.WriteLineAsync($"Part 2:\n{part2}");
+        });
+    }
+
+    (int part1, int part2) runPuzzle(IEnumerable<int> modules) {
+      return modules.Select(getRequiredFuel)
+        .Aggregate(
+          (0, 0),
+          ((int sum1, int sum2) s, (int fuel1, int fuel2) f) =>
+            (s.sum1 + f.fuel1, s.sum2 + f.fuel2)
+        );
+    }
+
+    (int fuelSimple, int fuelTyrannical) getRequiredFuel(int moduleWeight) {
+      var fuelSimple = fuelRequired(moduleWeight);
+      return (fuelSimple, fuelRecursive(fuelSimple, fuelSimple));
+    }
+
+    int fuelRequired(int weight) => weight / 3 - 2;
+
+    int fuelRecursive(int weight, int total) =>
+      fuelRequired(weight) switch {
+        var fuel when fuel <= 0 => total,
+        var fuel                => fuelRecursive(fuel, total + fuel)
+      };
+  }
+}
--- advent-of-net-core/advent-of-net-core/Program.cs
+++ advent-of-net-core/advent-of-net-core/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AdventOfCode2019 {
+
   class Program {
-    static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+    static Dictionary<int, Func<Task>> solvers = new Dictionary<int, Func<Task>> {
+      [1] = async () => await new Day01().solve()
+    };
+
+    static async Task Main(string[] args) {
+      var dayToRun = 0 < args.Length ? int.Parse(args[0]) : DateTime.Now.AddHours(-6).Day;
+      if (!solvers.TryGetValue(dayToRun, out var solve)) {
+        Console.Error.WriteLine($"Day {dayToRun} not supported.");
+        Environment.Exit(1);
+      }
+      Console.WriteLine($"Running day {dayToRun}.");
+      await solve();
     }
+
   }
 }
