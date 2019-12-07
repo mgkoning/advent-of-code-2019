2ef332d4ee6f2993c4643a459b6b40ce014f21c1
Michiel Koning <m.g.koning@gmail.com>
2019-12-07 10:53:11 +0100

    Day 07: Amplification Circuit (part 2)

--- advent-of-haskell/src/Day07.hs
+++ advent-of-haskell/src/Day07.hs
@@ -1,9 +1,10 @@
 module Day07 (solve) where
 
 import qualified Data.Vector.Unboxed as V
-import Intcode (runProgram)
+import Intcode (runProgram, Input, IntermediateState(..), Suspended(..), runWithSuspend)
 import Parsing (parseCommaSeparated, parseInt, resultOrError)
 import Data.List (permutations, maximum)
+import Data.Either (fromLeft, fromRight, isRight)
 
 runAmplifierPermutations program =
   let phases = permutations [0..4]
@@ -19,9 +20,33 @@ runAmplifier program phases =
       outputsE = runProgram program [phaseE, head $ outputsD]
   in head outputsE
 
+runFeedbackPermutations program =
+  let phases = permutations [5..9]
+      outputs = map (runFeedbackAmplifier program) phases
+  in maximum outputs
+
+runFeedbackAmplifier program phases =
+  let phaseA:phaseB:phaseC:phaseD:phaseE:_ = phases
+      amps = map (runWithSuspend program) $ map (:[]) phases
+      runCycle :: [IntermediateState] -> Input -> [IntermediateState]
+      runCycle (ampA:ampB:ampC:ampD:ampE:_) inputA =
+        let runAmp amp input = (resume $ fromLeft (error "Amp A not suspended") $ istate amp) input
+            getOutput amp = take 1 $ either currentOutput id $ istate amp
+            ampA' = runAmp ampA inputA
+            ampB' = runAmp ampB $ getOutput ampA'
+            ampC' = runAmp ampC $ getOutput ampB'
+            ampD' = runAmp ampD $ getOutput ampC'
+            ampE' = runAmp ampE $ getOutput ampD'
+            amps' = ampA':ampB':ampC':ampD':ampE':[]
+        in if isRight $ istate ampE' then amps' else runCycle amps' $ getOutput ampE'
+      (_:_:_:_:stateE:_) = runCycle amps [0]
+  in head $ fromRight (error "Program not completed") $ istate stateE
+
 solve = do
   program <- V.fromList <$>
              resultOrError <$> parseCommaSeparated parseInt <$>
              readFile "../input/day07.txt"
   putStrLn "Part 1:"
   print $ runAmplifierPermutations program
+  putStrLn "Part 2:"
+  print $ runFeedbackPermutations program
--- advent-of-haskell/src/Intcode.hs
+++ advent-of-haskell/src/Intcode.hs
@@ -1,43 +1,65 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Intcode (runSimple, Program, Input, Output, runProgram) where
+module Intcode (
+  runSimple,
+  Program, Input, Output, runProgram,
+  IntermediateState(..), Suspended(..), runWithSuspend
+) where
 
 import qualified Data.Vector.Unboxed as V
 
 type Program = V.Vector Int
 type Input = [Int]
 type Output = [Int]
-data State = State { input :: Input, program :: Program, output :: Output }
+
+data Suspended = Suspended { currentOutput :: Output, resume :: Input -> IntermediateState }
+newtype IntermediateState = IntermediateState { istate :: Either Suspended Output }
+
+data Mode = Running | WaitingForInput | Halted deriving (Eq, Show)
+data State = State { input :: Input, program :: Program, mode :: Mode, ip :: Int, output :: Output }
 
 runSimple :: Program -> Program
-runSimple program = finalProgram
-  where State { program = finalProgram } = doRunProgram (State [] program []) 0
+runSimple program = if mode == Halted then finalProgram else error ("Did not run until halted, mode: " ++ show mode)
+  where State { program = finalProgram, mode = mode } = doRunProgram (State [] program Running 0 [])
 
 runProgram :: Program -> Input -> Output
-runProgram program input = output
-  where (State {output = output}) = doRunProgram (State input program []) 0
+runProgram program input = if mode == Halted then output else error ("Did not run until halted, mode: " ++ show mode)
+  where (State {output = output, mode = mode}) = doRunProgram (State input program Running 0 [])
 
-doRunProgram :: State -> Int -> State
-doRunProgram state@(State {..}) pointer =
-  let current = program V.! pointer
+runWithSuspend :: Program -> Input -> IntermediateState
+runWithSuspend program input = runWithSuspend' $ State input program Running 0 []
+  where 
+    runWithSuspend' s =
+      let state@(State {output = output, mode = mode}) = doRunProgram s
+      in case mode of
+           Halted -> IntermediateState $ Right output
+           WaitingForInput -> 
+             let suspended = Suspended output $ \i -> runWithSuspend' state { input = i, mode = Running }
+             in IntermediateState $ Left suspended
+
+doRunProgram :: State -> State
+doRunProgram state@(State {..}) =
+  let current = program V.! ip
       (argModes, opcode) = current `divMod` 100
       (argModes', arg1Mode) = argModes `divMod` 10
       (argModes'', arg2Mode) = argModes' `divMod` 10
       (_, arg3Mode) = argModes'' `divMod` 10
       (headInput:input') = input
-      arg1 = let i = pointer + 1 in if arg1Mode == 0 then program V.! i else i
+      arg1 = let i = ip + 1 in if arg1Mode == 0 then program V.! i else i
       arg1Val = program V.! arg1
-      arg2 = let i = pointer + 2 in if arg2Mode == 0 then program V.! i else i
+      arg2 = let i = ip + 2 in if arg2Mode == 0 then program V.! i else i
       arg2Val = program V.! arg2
-      arg3 = let i = pointer + 3 in if arg3Mode == 0 then program V.! i else i
+      arg3 = let i = ip + 3 in if arg3Mode == 0 then program V.! i else i
   in case opcode of
-      1 -> doRunProgram state { program = (program V.// [(arg3, (arg1Val + arg2Val))]) } (pointer + 4)
-      2 -> doRunProgram state { program = (program V.// [(arg3, (arg1Val * arg2Val))]) } (pointer + 4)
-      3 -> doRunProgram (State input' (program V.// [(arg1, headInput)]) output) (pointer + 2)
-      4 -> doRunProgram state { output = arg1Val:output } (pointer + 2)
-      5 -> doRunProgram state (if arg1Val /= 0 then arg2Val else pointer + 3)
-      6 -> doRunProgram state (if arg1Val == 0 then arg2Val else pointer + 3)
-      7 -> doRunProgram state { program = (program V.// [(arg3, if arg1Val < arg2Val then 1 else 0)]) } (pointer + 4)
-      8 -> doRunProgram state { program = (program V.// [(arg3, if arg1Val == arg2Val then 1 else 0)]) } (pointer + 4)
-      99 -> state
-      x -> error ("unknown opcode " ++ (show x))
+      1 -> doRunProgram state { program = (program V.// [(arg3, (arg1Val + arg2Val))]), ip = ip + 4 }
+      2 -> doRunProgram state { program = (program V.// [(arg3, (arg1Val * arg2Val))]), ip = ip + 4 }
+      3 -> if input == []
+             then state { mode = WaitingForInput }
+             else doRunProgram state { input = input', program = (program V.// [(arg1, headInput)]), ip = ip + 2 }
+      4 -> doRunProgram state { output = arg1Val:output, ip = ip + 2 }
+      5 -> doRunProgram state { ip = if arg1Val /= 0 then arg2Val else ip + 3 }
+      6 -> doRunProgram state { ip = if arg1Val == 0 then arg2Val else ip + 3 }
+      7 -> doRunProgram state { program = (program V.// [(arg3, if arg1Val < arg2Val then 1 else 0)]), ip = ip + 4 }
+      8 -> doRunProgram state { program = (program V.// [(arg3, if arg1Val == arg2Val then 1 else 0)]), ip = ip + 4 }
+      99 -> state { mode = Halted }
+      x -> error ("unknown opcode " ++ show x)
