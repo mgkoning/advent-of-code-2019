24624c7f679d82779b62def0fd21a1ebad519ec5
Michiel Koning <m.g.koning@gmail.com>
2019-12-14 10:42:16 +0100

    Day 14: Space Stoichiometry (part 2)

--- advent-of-haskell/src/Day14.hs
+++ advent-of-haskell/src/Day14.hs
@@ -6,7 +6,6 @@ import Parsing
 import Text.Parsec (sepBy1, many1, letter, string)
 import Data.List ((\\), nub)
 import Data.HashMap.Strict (HashMap)
-import Data.Tuple (swap)
 import qualified Data.HashMap.Strict as M
 
 data Reaction = Reaction { reactants :: [Reagent], yield :: Reagent } deriving (Show)
@@ -18,6 +17,7 @@ parseReactions s = resultOrError $ parseLines parseReaction s
         mkReaction rs y = Reaction rs y
         mkReagent q m = Reagent q m
 
+{- Simple topological sort to determine the order of reaction requirements -}
 sortedReagents reactions =
   let edges = concatMap (\Reaction{..} -> zip (map material reactants) (repeat $ material yield)) reactions
   in topSort edges (noIncomingEdges edges) []
@@ -34,8 +34,10 @@ sortedReagents reactions =
             e' = e \\ toRemove
         in (e', filter (not . (`elem` (nub (map snd e')))) ms)
 
-requiredMaterials reactions sortedReagents =
-  foldl addRequirement (M.singleton "FUEL" 1) sortedReagents
+{- Given a list of reactions and the topologically sorted list of reagents,
+   determine the materials needed to get the required fuel amount -}
+requiredMaterials reactions sortedReagents requiredFuel =
+  foldl addRequirement (M.singleton "FUEL" requiredFuel) sortedReagents
   where reactionLookup = M.fromList $ map (\r@Reaction{..} -> (material yield, r)) reactions
         addRequirement requirements material =
           if not $ M.member material reactionLookup then requirements
@@ -46,9 +48,20 @@ requiredMaterials reactions sortedReagents =
                 factor = q + if 0 < r then 1 else 0
             in foldr (\Reagent{..} r -> M.insertWith (+) material (factor * quantity) r) requirements (reactants reaction)
 
+{- Approximate the number of materials required starting from a previous approximation by the given step -}
+getApprox reactions sorted from step =
+  let approximations = map (\i -> (i, (M.! "ORE") $ requiredMaterials reactions sorted i)) [from,from+step..]
+      bestApproximation = last $ takeWhile ((<=1000000000000) . snd) approximations
+  in fst bestApproximation
+
 solve = do
   reactions <- parseReactions <$> readFile "../input/day14.txt"
   putStrLn "Part 1:"
-  let allRequired = requiredMaterials reactions $ sortedReagents reactions
+  let sorted = sortedReagents reactions
+  let allRequired = requiredMaterials reactions sorted 1
   let oreRequired = allRequired M.! "ORE"
   print $ oreRequired
+  putStrLn "Part 2:"
+  let minimum = 1000000000000 `div` oreRequired
+  let maxFuel = foldl (getApprox reactions sorted) minimum [1000,100,10,1]
+  print maxFuel
