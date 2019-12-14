89ec88d47ee2274b7dcd78ef6756a3776eb1702e
Michiel Koning <m.g.koning@gmail.com>
2019-12-14 12:19:06 +0100

    Day 13: Care Package (part 2) Would you like to play a game?

--- advent-of-haskell/src/Day13.hs
+++ advent-of-haskell/src/Day13.hs
@@ -4,16 +4,18 @@ import Intcode
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as M
 import Data.List.Split (chunksOf)
-import Text.Printf
+import Text.Printf (printf)
 
 readOutputTriple [x,y,t] = ((x,y), t)
 readOutputTriple _ = error "not three outputs"
 
+toScreen :: [Int] -> HashMap (Int, Int) Int
 toScreen output =
   let outputs = map readOutputTriple $ chunksOf 3 output
-      drawnTiles = foldr (\next@(coord, tile) map -> M.insert coord tile map) M.empty outputs
+      drawnTiles = foldl (\ map next@(coord, tile) -> M.insert coord tile map) M.empty outputs
   in drawnTiles
 
+paintScreen :: HashMap (Int, Int) Int -> String
 paintScreen grid =
   let keys = M.keys grid
       maxX = maximum $ map fst keys
@@ -29,6 +31,27 @@ getTile t = case t of
               3 -> '_'
               4 -> '@'
 
+getCommands output paddlePos =
+  let (paddle, ball, output') = consume' paddlePos Nothing output
+      consume' (Just p) (Just b) o = (p, b, o)
+      consume' p b (x:y:t:os) =
+          case t of 4 -> consume' p (Just x) os
+                    3 -> consume' (Just x) b os
+                    _ -> consume' p b os
+      determineMove px bx
+        | bx < px = -1
+        | bx == px = 0
+        | px < bx = 1
+      move = determineMove paddle ball
+  in {- We have to keep track of the paddle in case we don't move it -}
+      move:(getCommands output' (if move == 0 then Just paddle else Nothing))
+
+finalScreen program =
+  let noQuarters = runProgram (memSet program 0 2) joystickCommands
+      joystickCommands = getCommands noQuarters Nothing
+  in toScreen noQuarters
+
+solve :: IO ()
 solve = do
   program <- readProgram <$> readFile "../input/day13.txt"
   putStrLn "Part 1:"
@@ -36,12 +59,6 @@ solve = do
   putStrLn $ paintScreen screen
   print $ length $ M.keys $ M.filter (==2) screen
   putStrLn "Part 2:"
-  --let runner = runProgram (memSet program 0 2) <$> (sequence $ repeat $ fmap toInput getChar)
-  --print "x"
-  --mapM_ putStrLn
-  --print $ runProgram (memSet program 0 2) []
-
-toInput c
-  | c == 'a' = -1
-  | c == 's' = 0
-  | c == 'd' = 1
+  let final = finalScreen program
+  putStrLn $ paintScreen final
+  print $ final M.! (-1, 0)
