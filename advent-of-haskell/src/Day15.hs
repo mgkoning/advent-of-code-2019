20ebd56df7d09c3579781d7a72a193c23fb145a4
Michiel Koning <m.g.koning@gmail.com>
2019-12-15 12:33:28 +0100

    Day 15: Oxygen System (part 2) with a print out of the map for giggles

--- advent-of-haskell/src/Day15.hs
+++ advent-of-haskell/src/Day15.hs
@@ -12,11 +12,6 @@ move (x, y) direction =
     3 -> (x-1, y) -- West
     4 -> (x+1, y) -- East
 
-makeTriple (a, b) c = (a,b,c)
-fst3 (a, b, c) = a
-snd3 (a, b, c) = b
-trd (a, b, c) = c
-
 buildMap program = buildMap' [((0,0), 0, startState)] M.empty Nothing
   where
     startState = State [] program 0 0
@@ -25,15 +20,26 @@ buildMap program = buildMap' [((0,0), 0, startState)] M.empty Nothing
     buildMap' [] visited shortest = (visited, shortest)
     buildMap' ((pos, steps, state):rest) visited shortest =
       let steps' = steps + 1
-          possibleMoves = zip3 allDirections (map (move pos) allDirections) (map runToStatus allDirections)
-          newMoves = filter (not . (`M.member` visited) . snd3) possibleMoves
+          possibleMoves = zip (map (move pos) allDirections) (map runToStatus allDirections)
+          newMoves = filter (not . (`M.member` visited) . fst) possibleMoves
           runToStatus n = head $ dropWhile (null . snd) $ doRunProgram state { input = [n] }
-          visited' = foldl (\m (_, p, (_, [s])) -> M.insert p s m) visited newMoves
-          knownGood = map (\(_, p, (state', _)) -> (p, steps', state')) $ filter ((==1) . head . snd . trd) newMoves
-          shortest' = if isNothing shortest && any ((== 2) . head . snd . trd) newMoves
-                        then Just steps'
-                        else shortest
+          visited' = foldl (\m (p, (_, [s])) -> M.insert p s m) visited newMoves
+          knownGood = map (\(p, (state', _)) -> (p, steps', state')) $ filter ((==1) . head . snd . snd) newMoves
+          shortest' = if any ((== 2) . head . snd . snd) newMoves then Just steps' else shortest
       in buildMap' (rest ++ knownGood) visited' shortest'
+
+fillWithOxygen :: HashMap (Int, Int) Int -> (Int, Int) -> Int
+fillWithOxygen areaMap startAt = fillWithOxygen' [(startAt, 0)] M.empty
+  where
+    allDirections = [1..4]
+    fillWithOxygen' [] visited = maximum $ M.elems visited
+    fillWithOxygen' ((pos, steps):rest) visited =
+      let allMoves = map (move pos) allDirections
+          newMoves = filter (not . (`M.member` visited)) allMoves
+          legalMoves = filter ((==1) . (areaMap M.!)) newMoves
+          steps' = steps + 1
+          visited' = foldl (\m p -> M.insert p steps' m) visited legalMoves
+      in fillWithOxygen' (rest ++ (zip legalMoves (repeat steps'))) visited'
 
 solve = do
   program <- readProgram <$> readFile "../input/day15.txt"
@@ -41,4 +47,20 @@ solve = do
   let (builtMap, shortestToOxygenUnit) = buildMap program
   print $ fromJust shortestToOxygenUnit
   let oxygenUnitLocation = fst $ head $ filter ((==2) . snd) $ M.toList builtMap
-  print oxygenUnitLocation
+  putStrLn "\nThe map:"
+  putStrLn $ printMap builtMap
+  putStrLn "Part 2:"
+  print $ fillWithOxygen builtMap oxygenUnitLocation
+
+{- Just for fun -}
+printMap grid =   
+  let keys = M.keys grid
+      minX = minimum $ map fst keys
+      maxX = maximum $ map fst keys
+      minY = minimum $ map snd keys
+      maxY = maximum $ map snd keys
+      getTile t = case t of 0 -> '#'
+                            1 -> '.'
+                            2 -> '@'
+      tiles = unlines $ [[getTile (M.lookupDefault 0 (x,y) grid) | x <- [minX..maxX]] | y <- [minY..maxY]]
+  in tiles
