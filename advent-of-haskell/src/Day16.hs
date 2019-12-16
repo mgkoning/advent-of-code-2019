344c784cf1f32b7b9a01739209fc2a6f94ccb28c
Michiel Koning <m.g.koning@gmail.com>
2019-12-16 08:02:09 +0100

    Day 16: Flawed Frequency Transmission (part 1)

---
+++ advent-of-haskell/src/Day16.hs
@@ -0,0 +1,36 @@
+module Day16 (solve, test) where
+
+import Data.Char (digitToInt, intToDigit)
+
+parseSignal s = map digitToInt s
+
+getMatrix :: [Int] -> [[Int]]
+getMatrix s =
+  let digitPatterns = map makePattern [1..]
+      signalLength = length s
+      makePattern i = take signalLength $ drop 1 $ cycle $ concatMap (replicate i) [0,1,0,-1]
+  in take signalLength digitPatterns
+
+applyFftPhase :: [[Int]] -> [Int] -> [Int]
+applyFftPhase matrix s = map (((`mod` 10) . abs) . sum . (zipWith (*) s)) matrix
+
+fft s =
+  let matrix = getMatrix s
+  in map intToDigit $ head $ drop 100 $ iterate (applyFftPhase matrix) s
+
+solve = do
+  input <- readFile "../input/day16.txt"
+  let signal = parseSignal input
+  putStrLn "Part 1:"
+  putStrLn $ take 8 $ fft signal
+  --putStrLn "Part 2:"
+  --let offset = (read (take 7 input)) :: Int
+  --print offset
+  --putStrLn $ take 8 $ drop offset $ fft $ concat $ replicate 10000 signal
+
+test = do
+  print $ take 5 $ iterate (applyFftPhase (getMatrix [1,2,3,4,5,6,7,8])) [1,2,3,4,5,6,7,8]
+  putStrLn $ take 8 $ fft $ parseSignal "80871224585914546619083218645595"
+  putStrLn $ take 8 $ fft $ parseSignal "19617804207202209144916044189917"
+  putStrLn $ take 8 $ fft $ parseSignal "69317163492948606335995924319873"
+
--- advent-of-haskell/src/Solvers.hs
+++ advent-of-haskell/src/Solvers.hs
@@ -16,12 +16,14 @@ import Day12
 import Day13
 import Day14
 import Day15
+import Day16
 
 solvers :: V.Vector (IO ())
 solvers = V.fromList [
   Day01.solve, Day02.solve, Day03.solve, Day04.solve, Day05.solve,
   Day06.solve, Day07.solve, Day08.solve, Day09.solve, Day10.solve,
-  Day11.solve, Day12.solve, Day13.solve, Day14.solve, Day15.solve]
+  Day11.solve, Day12.solve, Day13.solve, Day14.solve, Day15.solve,
+  Day16.solve]
 
 getSolver :: Int -> Maybe (IO ())
 getSolver day = solvers V.!? (day - 1)
