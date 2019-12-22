1d6ea9bee0efbd4f2e6e2fa774c299c51eb3ac48
Michiel Koning <m.g.koning@gmail.com>
2019-12-22 20:30:11 +0100

    Day 18: Many-Worlds Interpretation (part 1)

--- advent-of-haskell/src/Day18.hs
+++ advent-of-haskell/src/Day18.hs
@@ -1,16 +1,13 @@
 {-# LANGUAGE RecordWildCards #-}
-module Day18 (solve, test, combinations) where
+module Day18 (solve, test) where
 
 import qualified Data.Map.Strict as M
 import           Data.Map.Strict (Map)
 import           Data.Set (Set)
 import qualified Data.Set as S
 import           Data.Char (isLower, isUpper, toUpper, toLower)
-import           Data.List (minimumBy, delete, (\\))
+import           Data.List (minimumBy)
 import           Data.Ord (comparing)
-import           Data.Maybe (mapMaybe, isNothing, fromJust)
-import           Data.Tuple (swap)
-import           Debug.Trace (trace, traceShow, traceShowId)
 
 data Object = Entrance | Key Char | Door Char | Wall | Open deriving (Show, Eq)
 fromChar :: Char -> Object
@@ -63,94 +60,44 @@ isLegalMove cave next =
                     Entrance -> (True, S.empty)
                     Key _ -> (True, S.empty)
 
-ignoreDoors cave next = M.member next cave
-
-data State = State { sDist :: Int, sPos :: (Int, Int), sKeys :: Keys } deriving (Show, Eq)
-instance Ord State where (<=) (State d1 _ k1) (State d2 _ k2) = if S.size k1 == S.size k2 then d1 <= d2 else S.size k2 <= S.size k1
-getKeys State{..} = sKeys
-getDist State{..} = sDist
-
--- shortestVisitOrder :: HashMap (Int, Int) Object -> (Int, Int) -> [((Int, Int), Object)] -> Maybe State
--- shortestVisitOrder cave entrance targets = shortestVisitOrder' (S.singleton (State 0 entrance [])) S.empty Nothing
---   where
---     shortestVisitOrder' toVisit considered shortestSoFar = 
---       let (best@State{..}, v) = S.deleteFindMin toVisit
---           remaining = filter (not . (`elem` sKeys) . fromKey . snd) targets
---           allVisited = visitAll (isLegalMove cave (map Key sKeys)) sPos
---           visitedKeys = mapMaybe (\r@(p, o) -> fmap ((,) r) (M.lookup p allVisited)) remaining
---           newMoves = filter (not . (`S.member` considered) . getKeys) $
---             map (\((p, o), dist) -> let newKeys = (fromKey o):sKeys in State (sDist + dist) p newKeys) visitedKeys
---           tooLong = case shortestSoFar of Nothing -> False; Just x -> (getDist x) < sDist
---       in
---         if S.null toVisit then shortestSoFar
---         else if tooLong then shortestVisitOrder' v considered shortestSoFar
---         else if null remaining then
---           let s' = case shortestSoFar of Nothing -> Just best; Just x -> Just (min x best)
---           in traceShow s' $ shortestVisitOrder' v considered s'
---         else shortestVisitOrder' (foldr S.insert v newMoves) (foldr S.insert considered (map getKeys newMoves)) shortestSoFar
-
-haveEnoughKeys keys (_, required) = S.null (required S.\\ keys)
-
---prune v dist = S.filter ((<dist) . getDist) v
-
-combinations :: Int -> [a] -> [[a]]
-combinations 0 list = [[]]
-combinations _ [] = []
-combinations n (x:xs) = map (x:) (combinations (n-1) xs) ++ combinations n xs
+data State = State { sDist :: Int, atKey :: Char, sKeys :: Keys } deriving (Show, Eq)
+instance Ord State where (<=) (State d1 _ _) (State d2 _ _) = d1 <= d2
 
 getAllKeys cave =
   let entrance = head $ M.keys $ M.filter (==Entrance) cave
       keys = M.toList $ M.filter isKey cave
-      keyPosMap = M.fromList $ map (\(p, Key k) -> (k, p)) keys
       allKeys = map (fromKey . snd) keys
       keyCount = length allKeys
+      entranceToKeys = getKeyDistanceMap entrance
+      keyToKeys = M.fromList $ zip allKeys $ map getKeyDistanceMap (map fst keys)
       getKeyDistanceMap from =
         let allDistances = visitAll (isLegalMove cave) from
             otherKeys = filter ((/=from) . fst) keys
         in  M.fromList $ zip (map (fromKey . snd) otherKeys) $ map (allDistances M.!) (map fst otherKeys)
-      distancesToKeys =
-        let pois = entrance:(map fst keys)
-        in M.fromList $ zip pois $ map getKeyDistanceMap pois
-      bestPath :: Int -> Map (Char, Keys) (Int, Keys) -> (Int, Keys)
-      bestPath n previousOptima
-        | keyCount < n = minimum $ M.elems previousOptima
-        | otherwise =  let combs = combinations n allKeys -- [[Char]]
-                           toVisit = concat $ map (\c -> [(k, c) | k <- c]) combs
-                            
-                           shortest :: (Char, [Char]) -> ((Char, Keys), (Int, Keys))
-                           shortest (to, includes) = minimumBy (comparing $ fst . snd) $ map (getDistance to) (delete to includes)
-                             where getDistance to from = 
-                                     let (d, needed) = (distancesToKeys M.! (keyPosMap M.! from)) M.! to
-                                         (prefixDist, prefixKeys) = previousOptima M.! (from, S.fromList (delete to includes))
-                                     in ((to, S.fromList includes), toDistance (S.insert from prefixKeys) (d + prefixDist, needed))
-                           newOptima = M.fromList $ map shortest toVisit
-                       in bestPath (n+1) newOptima
-      toDistance :: Keys -> (Int, Keys) -> (Int, Keys)
-      toDistance haveKeys (dist, keys) = if S.null (keys S.\\ haveKeys) then (dist, haveKeys) else (10000000, S.empty)
-  in bestPath 2 $ M.fromList $ map (\k -> ((k, S.singleton k), (toDistance S.empty) ((distancesToKeys M.! entrance) M.! k))) allKeys
-    --shortestVisitOrder (S.singleton $ State 0 entrance S.empty) Nothing
-    --zip keys $ map (`M.lookup` (distances M.! entrance)) kPos
-      -- shortestVisitOrder toVisit bestSoFar =
-      --   let (best@State{..}, v) = S.deleteFindMin toVisit
-      --       remainingKeys = S.toList $ allKeys S.\\ sKeys
-      --       options = filter ((haveEnoughKeys sKeys) . snd) $
-      --         zip remainingKeys $ map ((distancesToKeys M.! sPos) M.!) $ remainingKeys
-      --       toVisit' = foldr S.insert v $ map (\(k, (d, _)) -> State (sDist + d) (keyPosMap M.! k) (S.insert k sKeys)) options
-      --   in if S.null toVisit then bestSoFar
-      --      else if maybe False ((<sDist) . getDist) bestSoFar then shortestVisitOrder v bestSoFar
-      --      else 
-      --        if S.size sKeys == S.size allKeys
-      --          then let newBest = maybe best (`min` best) bestSoFar
-      --               in traceShow newBest $ shortestVisitOrder (prune v $ getDist newBest) $ Just newBest
-      --        else shortestVisitOrder toVisit' bestSoFar
-  --     needed = M.toList $ M.filter isKey cave
-  -- in shortestVisitOrder cave entrance needed
+      reachable haveKeys (_, required) = S.null $ required S.\\ haveKeys
+      possibleStarts = S.fromList $ M.elems $ M.mapWithKey (\k (d, _) -> State d k (S.singleton k)) $ M.filter (reachable S.empty) $ entranceToKeys
+      findBestPath toVisit bestPaths = 
+        if S.null toVisit then
+          minimumBy (comparing snd) $
+          M.toList $ M.filterWithKey (\(_, keys) _ -> S.size keys == keyCount) bestPaths
+        else let (candidate@State{..}, v) = S.deleteFindMin toVisit
+                 posAndKeys = (atKey, sKeys)
+                 isBetterPath = case M.lookup posAndKeys bestPaths of Nothing -> True
+                                                                      Just d -> sDist < d
+                 bestPaths' = M.insert posAndKeys sDist bestPaths
+                 toVisit' = foldr S.insert v $
+                   M.elems $
+                   M.mapWithKey (\k (d, _) -> State (d + sDist) k (S.insert k sKeys)) $
+                   M.filter (reachable sKeys) (keyToKeys M.! atKey)
+             in if not isBetterPath then findBestPath v bestPaths
+                else findBestPath toVisit' bestPaths'
+  in findBestPath possibleStarts M.empty
 
 solve = do
   cave <- readCave <$> readFile "../input/day18.txt"
   putStrLn "Part 1:"
   let order = getAllKeys cave
-  print order
+  print $ snd order
 
 test = do
   putStrLn "smallExample1:"
